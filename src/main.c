0df80a269fb20640a2a3e975d171e1f2b4d35050
Alberto Damo <alberto.damo@stiga.com>
2026-09-03 11:34:45 +0200

    feat(main): moved execution of board_main in a thread

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
@@ -32,6 +33,48 @@ extern void new_host_stdout_print(wasm_exec_env_t env, const char * msg)
     printf("board print advanced: %s\n", msg);
 }
 
+void* pthread_func(void* arg)
+{
+    uintptr_t res = 1;
+    wasm_exec_env_t main_exec_env = arg;
+    wasm_exec_env_t th_exec_env = {0};
+    wasm_module_inst_t module_inst = {0};
+    wasm_function_inst_t board_main_f = {0};
+
+    wasm_runtime_init_thread_env();
+
+    if ( !wasm_runtime_thread_env_inited() )
+    {
+        GOTO_END_AND_CUSTOM_ERROR("thread env not inited");
+    }
+
+    if ( !(th_exec_env = wasm_runtime_spawn_exec_env(main_exec_env)) )
+    {
+        GOTO_END_AND_CUSTOM_ERROR("failed creating thread exec env");
+    }
+
+    if ( !(module_inst = wasm_runtime_get_module_inst(main_exec_env)) )
+    {
+        GOTO_END_AND_CUSTOM_ERROR("failed fetching module instance");
+    }
+
+    if ( !(board_main_f = wasm_runtime_lookup_function(module_inst, "board_main")) )
+    {
+        GOTO_END_AND_CUSTOM_ERROR("board main function not found");
+    }
+
+    if ( !wasm_runtime_call_wasm(th_exec_env, board_main_f, 0, NULL) )
+    {
+        GOTO_END_AND_CUSTOM_ERROR(wasm_runtime_get_exception(module_inst));
+    }
+
+
+    res =0;
+end:
+        wasm_runtime_destroy_thread_env();
+    return (void*) res;
+}
+
 int main(int argc, char *argv[])
 {
     const char* input_file = argv[1];
@@ -39,8 +82,10 @@ int main(int argc, char *argv[])
 
     wasm_module_t module = {0};
     wasm_module_inst_t module_inst = {0};
-    wasm_function_inst_t board_main_f = {0};
     wasm_exec_env_t main_exec_env = {0};
+    pthread_t th_id = {0};
+
+    uintptr_t err;
 
     char* buf = NULL;
     uint32_t file_buffer_size = 0;
@@ -84,15 +129,16 @@ int main(int argc, char *argv[])
     module_inst = wasm_runtime_instantiate(module, stack_size, heap_size, INPUT_ERROR_BUF);
     if ( !module_inst ) GOTO_END;
 
-    board_main_f = wasm_runtime_lookup_function(module_inst, "board_main");
-    if ( !board_main_f ) GOTO_END_AND_CUSTOM_ERROR("board main function not found");
-
     main_exec_env = wasm_runtime_create_exec_env(module_inst, stack_size);
+    if ( !main_exec_env) GOTO_END_AND_CUSTOM_ERROR("failed creating main_exec_env");
 
-    if ( !wasm_runtime_call_wasm(main_exec_env, board_main_f, 0, NULL) )
+    if( (err = pthread_create(&th_id, NULL, pthread_func, main_exec_env)) )
     {
-        GOTO_END_AND_CUSTOM_ERROR(wasm_runtime_get_exception(module_inst));
+        GOTO_END_AND_CUSTOM_ERROR(strerror(err));
     }
+
+    pthread_join(th_id, (void**) &err);
+    if( err ) GOTO_END;
 
 
     printf("done\n");
